84cd8af98123a1f1a419317527e6d8e70ccca8dc
Nikhil <Nikhil@LAPTOP-FHTATA1S>
2024-12-26 16:59:01 +0530

    employe jwt security

---
+++ employee-management-system/src/main/java/com/javatechie/entity/Employee.java
@@ -0,0 +1,95 @@
+package com.javatechie.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.yaml.snakeyaml.events.Event;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String dept;
+    private double salary;
+    private String email;
+    private String userName;
+    private String password;
+    private String roles; // ROLE_HR , ROLE_MANAGER
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(Integer id, String name, String dept, double salary, String email, String userName, String password,
+			String roles) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+		this.email = email;
+		this.userName = userName;
+		this.password = password;
+		this.roles = roles;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+    
+    
+    
+}
